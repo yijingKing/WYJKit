52aea6cab5cc5acab4ea2c8de30a72d8be481916
╰莪呮想好好宠Nǐつ <1091676312@qq.com>
2019-06-24 14:29:08 +0800

    1

---
+++ WYJKit/WYJCategory/UIKit/UIFont/UIFont+YJAdd.h
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ Copyright (K), 2019 - ~, ╰莪呮想好好宠Nǐつ 
+ 
+ Author:        ╰莪呮想好好宠Nǐつ (Wang Yijing)
+ E-mail:        1091676312@qq.com
+ GitHub:        https://github.com/MemoryKing
+ ********************************************************************************/
+
+
+#import <UIKit/UIKit.h>
+#import <CoreGraphics/CoreGraphics.h>
+#import <CoreText/CoreText.h>
+
+
+#define SizeScale   (WLessIphone6 ? - 1.5 : (WIPhone6 || WIPhoneX) ? 0 : 1.5)
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIFont (YJAdd)
+///< Whether the font is bold.
+@property (nonatomic, readonly) BOOL isBold NS_AVAILABLE_IOS(7_0);
+///< Whether the font is italic.
+@property (nonatomic, readonly) BOOL isItalic NS_AVAILABLE_IOS(7_0);
+///< Whether the font is mono space.
+@property (nonatomic, readonly) BOOL isMonoSpace NS_AVAILABLE_IOS(7_0);
+///< Whether the font is color glyphs (such as Emoji).
+@property (nonatomic, readonly) BOOL isColorGlyphs NS_AVAILABLE_IOS(7_0);
+///< Font weight from -1.0 to 1.0. Regular weight is 0.0.
+@property (nonatomic, readonly) CGFloat fontWeight NS_AVAILABLE_IOS(7_0);
+
+/** font adaoter */
++ (UIFont *)fontAdapter:(CGFloat)font;
+
+@end
+
+NS_ASSUME_NONNULL_END
