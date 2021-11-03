5f83b6d3196c7946e9f47d5e3407fd9e16c9af9f
祎 <1091676312@qq.com>
2021-11-03 11:20:25 +0800

    重定义宏,文件名

---
+++ WYJKitDemo/Pods/Target Support Files/Pods-WYJKitDemo-WYJKitDemoUITests/Pods-WYJKitDemo-WYJKitDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WYJKitDemo_WYJKitDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WYJKitDemo_WYJKitDemoUITests
+@end
---
+++ WYJKitDemo/Pods/Target Support Files/Pods-WYJKitDemoTests/Pods-WYJKitDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WYJKitDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_WYJKitDemoTests
+@end
